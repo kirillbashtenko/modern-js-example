69ba4e4f49a98eb49f8ee2c7448b11e7246942a9
Kyrylo Bashtenko <kbashtenko@omilia.com>
2023-04-19 17:48:26 +0300

    feat: init

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,23 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
+import garfishPlugin from '@modern-js/plugin-garfish';
 
-// https://modernjs.dev/en/configure/app/usage
-export default defineConfig<'rspack'>({
+export default defineConfig({
   runtime: {
     router: true,
+    masterApp: {
+      apps: [
+        {
+          name: 'Table',
+          entry: 'http://localhost:8081',
+          // activeWhen: '/table'
+        },
+        {
+          name: 'Dashboard',
+          entry: 'http://localhost:8082',
+          // activeWhen: '/dashboard'
+        },
+      ],
+    },
   },
-  plugins: [
-    appTools({
-      bundler: 'experimental-rspack',
-    }),
-  ],
+  plugins: [appTools(), garfishPlugin()],
 });
