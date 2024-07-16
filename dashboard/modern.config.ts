fb1046fa5b942dea470109ea2f41bd06d03307e4
Kyrylo Bashtenko <kbashtenko@omilia.com>
2024-07-16 13:41:14 +0200

    feat: update to latest modern.js

--- dashboard/modern.config.ts
+++ dashboard/modern.config.ts
@@ -1,5 +1,5 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
-import garfishPlugin from '@modern-js/plugin-garfish';
+import { garfishPlugin } from '@modern-js/plugin-garfish';
 
 export default defineConfig<'rspack'>({
   dev: {
--- modern.config.ts
+++ modern.config.ts
@@ -1,12 +1,10 @@
 import appTools, { defineConfig } from '@modern-js/app-tools';
-import garfishPlugin from '@modern-js/plugin-garfish';
+import { garfishPlugin } from '@modern-js/plugin-garfish';
 
 export default defineConfig({
   runtime: {
     router: true,
-    masterApp: {
-      apps: [],
-    },
+    masterApp: true,
   },
   plugins: [appTools(), garfishPlugin()],
 });
